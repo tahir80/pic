3c49e89cdf962679a8877c210229e35b60d9f6cf
Tahir <tahir.abbas.80@gmail.com>
2024-07-31 17:36:14 +0200

    working with django admin

--- pic/admin.py
+++ pic/admin.py
@@ -5,30 +5,161 @@ from django.contrib import admin
 from pic.stat_analysis.models.report import Report
 from pic.stat_analysis.models.statistics import JobReportResult, OrderReportResult
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from pic.models import User, AccountManager, Customer, ServiceProvider, Status, \
+from pic.models import User, AccountManager, Customer, ServiceProvider, \
     AccountManagerCustomer, Service, Order, AccountManagerService, ServiceOrder
+from django import forms
 
 
-class UserAdmin(BaseUserAdmin):
+admin.site.site_header = "PICS ADMIN PANEL"
+
+
+class UserAdmin(admin.ModelAdmin):
     model = User
-    list_display = ('username', 'user_type', 'email') 
 
-    fieldsets = (
-        (None, {'fields': ('username', 'password', 'user_type', 'email')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    search_fields = ('username', )
+
+    list_display = ('username', 'user_type', 'email', 'phone')
+
+    list_filter = ['user_type']
+
+    list_display_links = ['username', 'user_type']
+
+    list_editable = ('email', 'phone', )
+
+    # fields = ('username',
+              
+    #           )
+
+
+class AccountManagerCustomerForm(forms.ModelForm):
+  
+    class Meta:
+        model = AccountManagerCustomer
+        fields = ['f_am_id', 'f_cus_id', 'date_assigned']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Print the queryset for debugging
+        print('AccountManager queryset:', AccountManager.objects.all())
+        print('Customer queryset:', Customer.objects.all())
+
+        account_managers = AccountManager.objects.all()
+        customers = Customer.objects.all()
+
+
+       # Get account manager and customer instances
+        account_managers = AccountManager.objects.select_related('user').all()
+        customers = Customer.objects.select_related('user').all()
+
+        # Print the fetched AccountManager instances for debugging
+        print('Account Managers:')
+        for am in account_managers:
+            user = am.user
+            print(f'ID: {am.am_id}, Name: {user.username}, Email: {user.email}, Phone: {user.phone}')
+
+        # Print the fetched Customer instances for debugging
+        print('Customers:')
+        for cus in customers:
+            user = cus.user
+            print(f'ID: {cus.cus_id}, Name: {user.username}, Email: {user.email}, Phone: {user.phone}')
+        
+
+        # Set choices for f_am_id
+        self.fields['f_am_id'].choices = [
+            (am.am_id, am.am_id) for am in account_managers
+        ]
+
+        # Set choices for f_cus_id
+        self.fields['f_cus_id'].choices = [
+            (cus.cus_id, cus.cus_id) for cus in customers
+        ]
+
+
+class AccountManagerCustomerAdmin(admin.ModelAdmin):
+    form = AccountManagerCustomerForm
+    list_display = ('am_cust_id', 'get_am_username', 'get_cus_username', 'date_assigned')
+
+    def get_am_username(self, obj):
+        return obj.f_am_id.user.username if obj.f_am_id and obj.f_am_id.user else 'No Account Manager'
+    get_am_username.short_description = 'Account Manager Username'
+
+    def get_cus_username(self, obj):
+        return obj.f_cus_id.user.username if obj.f_cus_id and obj.f_cus_id.user else 'No Customer'
+    get_cus_username.short_description = 'Customer Username'
+
+admin.site.register(AccountManagerCustomer, AccountManagerCustomerAdmin)
+
+
+
+class AccountManagerAdmin(admin.ModelAdmin):
+    list_display = (
+        'am_id', 
+        'get_user_username', 
+        'get_email', 
+        'get_phone'
     )
-    add_fieldsets = (
-        (None, {'classes': ('wide',), 'fields': ('username', 'user_type', 'password1', 'password2', 'email')}),
-    )
+
+    def get_user_username(self, obj):
+        return obj.user.username if obj.user else None
+    get_user_username.short_description = 'Username'
+
+    def get_email(self, obj):
+        return obj.user.email if obj.user else None
+    get_email.short_description = 'Email'
+
+    def get_phone(self, obj):
+        return obj.user.phone if obj.user else None
+    get_phone.short_description = 'Phone'
+
+admin.site.register(AccountManager, AccountManagerAdmin)
+
+
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('cus_id', 'get_user_username', 'get_phone', 'get_email')
+
+    # Method to display the username of the Customer
+    def get_user_username(self, obj):
+        return obj.user.username if obj.user else None
+    get_user_username.short_description = 'Username'  
+
+    def get_phone(self, obj):
+        return obj.user.phone if obj.user else None
+    get_phone.short_description = 'Phone'
+
+    def get_email(self, obj):
+       return obj.user.email if obj.user else None
+    get_email.short_description = 'Email'
+
+admin.site.register(Customer, CustomerAdmin)
 
 admin.site.register(User, UserAdmin)
-admin.site.register(AccountManager)
-admin.site.register(Customer)
-admin.site.register(ServiceProvider)
-admin.site.register(Status)
-admin.site.register(AccountManagerCustomer)
+# admin.site.register(AccountManager)
+# admin.site.register(Customer)
+
+class ServiceProviderAdmin(admin.ModelAdmin):
+    list_display = ('get_user_username', 'get_email', 'get_phone')
+
+    # fields = ("name", "email", "phone", )
+    def get_user_username(self, obj):
+        return obj.user.username if obj.user else None
+    get_user_username.short_description = 'Username'
+
+    def get_phone(self, obj):
+        return obj.user.phone if obj.user else None
+    get_phone.short_description = 'Phone'
+
+    def get_email(self, obj):
+       return obj.user.email if obj.user else None
+    get_email.short_description = 'Email'  
+
+admin.site.register(ServiceProvider, ServiceProviderAdmin)
+# admin.site.register(Status)
+# admin.site.register(AccountManagerCustomer)
+
+
 admin.site.register(Service)
 admin.site.register(Order)
 admin.site.register(AccountManagerService)
--- picproject/settings.py
+++ picproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pic',
-    'django_seed'
+    'django_seed',
 ]
 
 MIDDLEWARE = [
